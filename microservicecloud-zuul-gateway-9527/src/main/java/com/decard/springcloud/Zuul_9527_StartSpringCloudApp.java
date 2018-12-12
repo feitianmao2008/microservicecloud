8ae0cc79c5101642efd2fa8d279c782cf8f8a4e2
feitianmao2008 <284850653@qq.com>
2018-12-12 20:59:29 +0800

    gateway

---
+++ microservicecloud-zuul-gateway-9527/src/main/java/com/decard/springcloud/Zuul_9527_StartSpringCloudApp.java
@@ -0,0 +1,26 @@
+/**
+ * **********************************************************************************************************
+ * 版权声明：该软件的知识产权归深圳德卡股份有限公司所有。
+ * Copyright Statement: The software intellectual property is owned by ShenZhen Decard SmartCard Tech Co.,Ltd.
+ * **********************************************************************************************************
+ */
+package com.decard.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author dcb 284850653@qq.com
+ * @version 1.0
+ * @description 类作用描述
+ * @createDate 2018/12/12 13:44
+ */
+@SpringBootApplication
+@EnableZuulProxy
+public class Zuul_9527_StartSpringCloudApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Zuul_9527_StartSpringCloudApp.class, args);
+    }
+}
