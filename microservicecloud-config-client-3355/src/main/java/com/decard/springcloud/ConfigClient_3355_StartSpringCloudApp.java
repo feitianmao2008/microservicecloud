376dbab289fcd6cc33e3ab790f41a330c472a5ce
dcb <284850653@qq.com>
2018-12-14 08:21:13 +0800

    config

---
+++ microservicecloud-config-client-3355/src/main/java/com/decard/springcloud/ConfigClient_3355_StartSpringCloudApp.java
@@ -0,0 +1,15 @@
+package com.decard.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Administrator on 2018/12/13.
+ */
+@SpringBootApplication
+public class ConfigClient_3355_StartSpringCloudApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClient_3355_StartSpringCloudApp.class, args);
+    }
+}
